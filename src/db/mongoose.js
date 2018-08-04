3ef1c0a8961a0bbf9b6a0ab53d4baa56f41e6d68
neopanther <amaiplusplus@gmail.com>
2018-08-04 06:53:38 +0200

    Prep repo for Heroku: setting database for Heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-var url process.env.DATABASEURL || 'mongodb://localhost:27017/microblog'
-mongoose.connect(url);
+var url process.env.DATABASEURL || "mongodb://localhost:27017/microblog";
+mongoose.connect(url, {useMongoClient: true});
 
 module.exports = {mongoose};
