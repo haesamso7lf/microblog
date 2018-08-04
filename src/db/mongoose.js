5e3aab7b70493487e48a812cc7c4bf90570970d4
neopanther <amaiplusplus@gmail.com>
2018-08-04 07:11:23 +0200

    setting database for Heroku: syntax err

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-var url process.env.DATABASEURL || "mongodb://localhost:27017/microblog";
-mongoose.connect(url, {useMongoClient: true});
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/microblog";
+mongoose.connect(url);
 
 module.exports = {mongoose};
