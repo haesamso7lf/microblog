8b73e6f5586a50ae7fe86b3cfde160543a1f91f0
neopanther <amaiplusplus@gmail.com>
2018-08-04 18:01:37 +0200

    connceting with MLab

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/microblog";
+var url = process.env.DATABASEURL || "mongodb://microblog123:microblog123@ds111192.mlab.com:11192/microblog";
 mongoose.connect(url);
 
 module.exports = {mongoose};
